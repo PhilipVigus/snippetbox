ec48ce83036598974f78aa23c582a33482301453
Phil Vigus <philvigus@gmail.com>
2024-10-07 18:57:57 +0200

    Add create and view routes

--- main.go
+++ main.go
@@ -10,10 +10,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	// Register the home function as the handler for the "/" URL pattern
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Print("Starting server on :4000")
 	// Start the web server using the default ServeMux as the router
 	// we aren't specifying a host, so the server will listen on all available interfaces on the machine

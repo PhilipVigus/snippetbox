a31dff32a78719657eae028d9d129abbc41127a0
Phil Vigus <philvigus@gmail.com>
2024-10-10 21:12:08 +0200

    Fix app start timing issue so db has time to init

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,10 +18,38 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "root:password@tcp(db:3306)/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	var db *sql.DB // Declare db variable outside the loop
+
+	for i := 0; i < 50; i++ {
+		var err error
+		db, err = openDB(*dsn)
+		if err == nil {
+			err = db.Ping()
+		}
+		if err == nil {
+			break
+		}
+		log.Println("Failed to connect to database, retrying in 2 seconds...")
+		time.Sleep(2 * time.Second)
+	}
+
+	if db != nil {
+		defer func() {
+			err := db.Close()
+			if err != nil {
+				log.Println("Error closing database connection:", err)
+			}
+		}()
+	} else {
+		log.Fatal("Unable to connect to the database after multiple attempts")
+	}
 
 	app := &application{
 		errorLog: errorLog,
@@ -34,4 +66,17 @@ func main() {
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }

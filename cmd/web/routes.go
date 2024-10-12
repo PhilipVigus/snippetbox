cd092b6f5eae56742ea6d41b2b67bdc7470ef406
Phil Vigus <philvigus@gmail.com>
2024-10-12 10:49:09 +0200

    Fix route handler bug

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,7 +9,7 @@ func (app *application) routes() *http.ServeMux {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetCreate)
+	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	return mux
